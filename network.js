aade8dac644a3d29ffef6bd06189be2df3fe9f09
AlexOnomy <alex@onomy.io>
2022-01-17 20:16:29 +0000

    Initial onomy-testnet config

--- network.js
+++ network.js
@@ -1,26 +1,24 @@
 export default {
-  id: 'cosmos-hub-mainnet',
-  name: 'Cosmos Mainnet',
+  id: 'onomy-testnet', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
+  name: 'Onomy Testnet',
   description:
-    'Cosmos is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
+    'Onomy Protocol is a layer-1 Cosmos-based blockchain network powering a cross-chain orderbook + AMM decentralized exchange (DEX) and a stablecoin issuance system',
   logo: `logo.svg`,
-  website: 'https://cosmos.network',
-  // apiURL: 'http://localhost:8010/proxy',
-  apiURL: 'https://api.cosmos.network',
-  rpcURL: 'https://rpc.cosmos.network',
-  minBlockHeight: 5200792, // actually 5200791, but it has the wrong block time.
-  stakingDenom: 'ATOM',
+  website: 'https://onomy.io',
+  apiURL: 'https://rest-testnet.onomy.io', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  rpcURL: 'wss://rpc-testnet.onomy.io',
+  stakingDenom: 'NOM',
   coinLookup: [
     {
-      viewDenom: 'ATOM',
-      chainDenom: 'uatom',
-      chainToViewConversionFactor: 1e-6,
-      icon: `currencies/atom.png`,
+      viewDenom: 'NOM',
+      chainDenom: 'anom',
+      chainToViewConversionFactor: 1e-18,
+      icon: `currencies/muon.png`,
     },
   ],
-  addressPrefix: 'cosmos',
-  validatorAddressPrefix: 'cosmosvaloper',
-  validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
+  addressPrefix: 'onomy',
+  validatorAddressPrefix: 'onomyvaloper',
+  validatorConsensusaddressPrefix: 'onomyvalcons', // needed to map validators from staking queries to the validator set
   HDPath: `m/44'/118'/0'/0/0`,
   lockUpPeriod: `3 days`,
   fees: {
@@ -28,12 +26,15 @@ export default {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'ATOM',
+          denom: 'NOM',
           amount: 0.001,
         },
       ],
     },
   },
   icon: `https://lunie.fra1.digitaloceanspaces.com/network-icons/cosmos.png`,
-  localSigning: false, // this is only to be used as a developer tool - never deployed in production or for mainnet chains
+
+  // This is only to be used as a developer tool and for testing purposes
+  // NEVER ENABLE LOCALSIGNING IN PRODUCTION OR FOR MAINNETS
+  localSigning: false,
 }
